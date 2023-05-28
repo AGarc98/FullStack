bec255330ee8bce7a83d7bc2704fcc24ba475b48
Alexander Garcia <nightrix94@hotmail.com>
2023-05-27 18:39:07 -0700

    feat(JSON template): created webpage that uses json data

--- travlr/app.js
+++ travlr/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
+const hbs = require('hbs');
 
 var app = express();
 
